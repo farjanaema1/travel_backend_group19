5b20d3c538cf700959a21adffee5619226d23bab
farjana ema <farjanaema37@gmail.com>
2025-08-19 16:32:50 +0600

    update

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const nodemailer = require('nodemailer');
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.use(cors({
+  origin: ["https://travel-guro-19.vercel.app/"], 
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 
 // Middleware
 app.use(cors());
